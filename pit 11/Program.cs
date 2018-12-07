34538ed2f7b61c8af5113c4fb805e906913279bd
Roksana <roksana@1450.pl>
2018-12-07 14:15:07 +0100

    Comit Roxi

--- pit 11/Program.cs
+++ pit 11/Program.cs
@@ -23,11 +23,12 @@ class Pit11
 		//indywidualnie
 		radioButton = driver.FindElement(By.CssSelector("#rd1"));
 		radioButton.Click();
+		Thread.Sleep(200);
 		Console.WriteLine(radioButton.GetAttribute("value"));
 		zielonaFajka = driver.FindElement(By.CssSelector("#rd1pipe"));
 		if (zielonaFajka.Displayed)
 		{
-			Console.WriteLine("Pojawiłą się zielona fajka");
+			Console.WriteLine("Pojawiła się zielona fajka");
 		}
 		else
 		{
@@ -36,11 +37,12 @@ class Pit11
 		//wspolnie z malzonkiem
 		radioButton = driver.FindElement(By.CssSelector("#rd2"));
 		radioButton.Click();
+		Thread.Sleep(200);
 		Console.WriteLine(radioButton.GetAttribute("value"));
 		zielonaFajka = driver.FindElement(By.CssSelector("#rd2pipe"));
 		if (zielonaFajka.Displayed)
 		{
-			Console.WriteLine("Pojawiłą się zielona fajka");
+			Console.WriteLine("Pojawiła się zielona fajka");
 		}
 		else
 		{
@@ -48,6 +50,7 @@ class Pit11
 		}
 		informator = driver.FindElement(By.CssSelector("#frame_1_1 > fieldset:nth-child(2) > div:nth-child(3) > a > img"));
 		informator.Click();
+		Thread.Sleep(200);
 		if (informator.Enabled)
 		{
 			Console.WriteLine("Okienko z informacja się otworzyło");
@@ -62,33 +65,102 @@ class Pit11
 		//wspolnie ze zmarlym malzonkiem
 		radioButton = driver.FindElement(By.CssSelector("#rd3"));
 		radioButton.Click();
+		Thread.Sleep(200);
 		Console.WriteLine(radioButton.GetAttribute("value"));
 		zielonaFajka = driver.FindElement(By.CssSelector("#rd3pipe"));
 		if (zielonaFajka.Displayed)
 		{
-			Console.WriteLine("Pojawiłą się zielona fajka");
+			Console.WriteLine("Pojawiła się zielona fajka");
 		}
 		else
 		{
 			Console.WriteLine("Nie pojawiła się zielona fajka");
 		}
-
+		informator = driver.FindElement(By.CssSelector("fieldset.form_group:nth-child(2) > div:nth-child(4) > a:nth-child(3) > img:nth-child(1)"));
+		informator.Click();
+		Thread.Sleep(2000);
+		if (informator.Enabled)
+		{
+			Console.WriteLine("Okienko z infromacja otworzyło się");
+			informator = driver.FindElement(By.CssSelector(".s_close_btn > img:nth-child(1)"));
+			informator.Click();
+		}
+		else
+		{
+			Console.WriteLine("Brak dostępu");
+		}
 		//w sposob przewidziany dla ....
 		radioButton = driver.FindElement(By.CssSelector("#rd4"));
 		radioButton.Click();
+		Thread.Sleep(200);
 		Console.WriteLine(radioButton.GetAttribute("value"));
 		zielonaFajka = driver.FindElement(By.CssSelector("#rd4pipe"));
 		if (zielonaFajka.Displayed)
 		{
-			Console.WriteLine("Pojawiłą się zielona fajka");
+			Console.WriteLine("Pojawiła się zielona fajka");
 		}
 		else
 		{
 			Console.WriteLine("Nie pojawiła się zielona fajka");
 		}
+		informator = driver.FindElement(By.CssSelector("fieldset.form_group:nth-child(2) > div:nth-child(4) > a:nth-child(3) > img:nth-child(1)"));
+		informator.Click();
+		if (informator.Enabled)
+		{
+			Console.WriteLine("Okienko z informacją otworzyło się");
+			informator = driver.FindElement(By.CssSelector(".s_close_btn > img:nth-child(1)"));
+			informator.Click();
+
+		}
+		else
+		{
+			Console.WriteLine("Brak dostępu");
+		}
 
 
+		//czy chcesz zlozyc zeznanie
 
+		radioButton = driver.FindElement(By.CssSelector("#zeznanie"));
+		radioButton.Click();
+		Thread.Sleep(2000);
+		Console.WriteLine("zlozenie zeznania");
+		zielonaFajka = driver.FindElement(By.CssSelector("#correctPipe"));
+		if (zielonaFajka.Displayed)
+		{
+			Console.WriteLine("Pojawiła się zielona fajka");
+		}
+		else
+		{
+			Console.WriteLine("Nie pojawiła się zielona fajka");
+		}
+		
+		// zlozenie korekty
+
+		radioButton = driver.FindElement(By.CssSelector("#correct"));
+		radioButton.Click();
+		Thread.Sleep(2000);
+		Console.WriteLine("zlozenie korekty zeznania");
+		zielonaFajka = driver.FindElement(By.CssSelector("#correctPipe"));
+		if (zielonaFajka.Displayed)
+		{
+			Console.WriteLine("Pojawiła się zielona fajka");
+		}
+		else
+		{
+			Console.WriteLine("Nie pojawila sie zielona fajka");
+		}
+		informator = driver.FindElement(By.CssSelector("fieldset.form_group:nth-child(3) > div:nth-child(2) > a:nth-child(5) > img:nth-child(1)"));
+		informator.Click();
+		if (informator.Enabled)
+		{
+			Console.WriteLine("Pojawiła się informacja");
+			informator = driver.FindElement(By.CssSelector(".s_close_btn > img:nth-child(1)"));
+			informator.Click();
+		}
+		else
+		{
+			Console.WriteLine("Brak dostępu");
+		}
 
 		Thread.Sleep(20000);
 		driver.Quit();
