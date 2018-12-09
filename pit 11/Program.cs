db5f900ab29f19c7cca20ff0e1ee39775890c311
Roksana <roksana1495@wp.pl>
2018-12-09 03:52:30 +0100

    dodanie 2 pita

--- pit 11/Program.cs
+++ pit 11/Program.cs
@@ -11,6 +11,12 @@ class Pit11
 	static IWebElement radioButton;
 	static IWebElement zielonaFajka;
 	static IWebElement informator;
+	static IWebElement szukajUrząd;
+	static IWebElement id;
+	static IWebElement textbox;
+	static IWebElement urzędy;
+	static IWebElement button;
+	static IWebElement checkbox;
 
 
 	static void Main()
@@ -61,7 +67,7 @@ class Pit11
 		{
 			Console.WriteLine("NIE OTWIERA SIĘ");
 		}
-		
+
 		//wspolnie ze zmarlym malzonkiem
 		radioButton = driver.FindElement(By.CssSelector("#rd3"));
 		radioButton.Click();
@@ -133,7 +139,7 @@ class Pit11
 		{
 			Console.WriteLine("Nie pojawiła się zielona fajka");
 		}
-		
+
 		// zlozenie korekty
 
 		radioButton = driver.FindElement(By.CssSelector("#correct"));
@@ -162,8 +168,137 @@ class Pit11
 			Console.WriteLine("Brak dostępu");
 		}
 
-		Thread.Sleep(20000);
-		driver.Quit();
+		textbox = driver.FindElement(By.Name("reason"));
+		textbox.Clear();
+		textbox.SendKeys("KOREKTA");
+		Thread.Sleep(2000);
+
+
+
+
+
+		//Urzad Skarbowy
+		//Sprawdzenie boxu tekstowego czy mozna cos wpisac
+
+		//szukajUrząd = driver.FindElement(By.Name("SelectedOfficeString"));
+		//szukajUrząd.SendKeys("Urząd Skarbowy w Jaworze");
+		//Thread.Sleep(2000);
+		//szukajUrząd.Clear();
+
+		//test wybrania urzedu i sprawdzenie czy to samo sie wyswietlilo u gory i czy jest fajka zielona
+
+		szukajUrząd = driver.FindElement(By.CssSelector("#TaxOfficesSelect"));
+		for (int i = 1; i < 5; i++)
+		{
+			urzędy = driver.FindElement(By.CssSelector("#TaxOfficesSelect > option:nth-child(" + i + ")"));
+			urzędy.Click();
+			Thread.Sleep(2000);
+			id = driver.FindElement(By.CssSelector("#selected_us"));
+			if (id.Displayed)
+
+			{
+				Console.WriteLine("zaznaczony urząd " + urzędy.GetAttribute("value"));
+			}
+			else
+			{
+				Console.WriteLine("Brak wyswietlenia");
+			}
+			zielonaFajka = driver.FindElement(By.CssSelector("#USpipe"));
+			if (zielonaFajka.Displayed)
+			{
+				Console.WriteLine("Pojawiła się zielona fajka");
+			}
+			else
+			{
+				Console.WriteLine("Nie pojawiła się zielona fajka");
+			}
+		}
+
+		button = driver.FindElement(By.CssSelector("#forward_btn"));
+		button.Click();
+
+		//Druga strona
+
+		//Wybor ilosci pit-11
+
+		radioButton = driver.FindElement(By.Id("PIT11:-1"));
+		radioButton.Click();
+		Thread.Sleep(3000);
+		for (int i = 1; i < 9; i++)
+		{
+			radioButton = driver.FindElement(By.Id("PIT11:" + i + ""));
+			radioButton.Click();
+			Console.WriteLine(radioButton.GetAttribute("checked"));
+		}
+
+		//czy podatnik uzyskal inne przychody niz uzyskane
+
+		radioButton = driver.FindElement(By.Id("Przychody:Nie"));
+		radioButton.Click();
+		Console.WriteLine(radioButton.GetAttribute("checked"));
+
+		radioButton = driver.FindElement(By.Id("Przychody:Tak"));
+		radioButton.Click();
+		Console.WriteLine(radioButton.GetAttribute("checked"));
+
+		//inne przychody
+
+		checkbox = driver.FindElement(By.CssSelector("Przychody3a DzialalnoscGospodarcza"));
+		checkbox.Click();
+		Console.WriteLine(checkbox.GetAttribute("value"));
+		
+		checkbox = driver.FindElement(By.Id("Przychody:RentyEmerytury"));
+		checkbox.Click();
+		checkbox.Click();
+		checkbox = driver.FindElement(By.Id("Przychody:PapieryWartosciowe"));
+		checkbox.Click();
+		checkbox.Click();
+		checkbox = driver.FindElement(By.Id("Przychody:RyczaltDzialalnosc"));
+		checkbox.Click();
+		checkbox.Click();
+		checkbox = driver.FindElement(By.Id("Przychody:SprzedazNieruchomosciDo2009"));
+		checkbox.Click();
+		checkbox.Click();
+		checkbox = driver.FindElement(By.Id("Przychody:SprzedazNieruchomosciOd2009"));
+		checkbox.Click();
+		checkbox.Click();
+		checkbox = driver.FindElement(By.Id("Przychody:NajemDzierzawaOgolne"));
+		checkbox.Click();
+		checkbox.Click();
+		checkbox = driver.FindElement(By.Id("Przychody:NajemDzierzawaRyczalt"));
+		checkbox.Click();
+		checkbox.Click();
+		checkbox = driver.FindElement(By.Id("Przychody:DzialySpecjalne"));
+		checkbox.Click();
+		checkbox.Click();
+		checkbox = driver.FindElement(By.Id("Przychody:MaloletnieDzieci"));
+		checkbox.Click();
+		checkbox.Click();
+		checkbox = driver.FindElement(By.Id("Przychody:ZbycieRzeczy"));
+		checkbox.Click();
+		checkbox.Click();
+		checkbox = driver.FindElement(By.Id("Przychody:InneZPIT8C"));
+		checkbox.Click();
+		checkbox.Click();
+		checkbox = driver.FindElement(By.Id("Przychody:Inne"));
+		checkbox.Click();
+		checkbox.Click();
+		checkbox = driver.FindElement(By.Id("Przychody:Zagranica"));
+		checkbox.Click();
+		checkbox.Click();
+
+
+
+
+
+
+
+
+
+		Thread.Sleep(2000);
+
+
+
 
 
 
